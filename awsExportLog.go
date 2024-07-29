d39bca693467f08f564a35b70f75ce0d5083bec0
Raymond Chiu <onionhead0708@gmail.com>
2024-07-29 21:49:02 +0100

    Add -o parameter for export log to a folder, Add -pre to specific the prefix of the stream name
    Either -s or -pre is mandatory

--- awsExportLog.go
+++ awsExportLog.go
@@ -12,36 +12,67 @@ import (
 )
 
 const (
-	VERSION   = "1.3"
+	VERSION   = "1.4"
 	ERROR_MSG = "ERROR: %s\n"
 )
 
 var (
-	profile       string
-	region        string
-	logEventInput *cloudwatchlogs.GetLogEventsInput
-	logTimestamp  bool
+	profile          string
+	region           string
+	logGroupName     string
+	logStreamName    string
+	logStreamPreix   string
+	startTime        int64
+	endTime          int64
+	showLogTimestamp bool
+	outfolder        string
 )
 
 func main() {
 	readConfigFromCommandLine()
-	retrieveAwsLog()
+	client, err := createAwsClient()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, ERROR_MSG, err)
+		os.Exit(1)
+	}
+
+	streams := []string{}
+	// streams := make([]string, 0)
+
+	// if the logStreamName ends with *, then get all the streams
+	// and append them to the streams list
+	if logStreamPreix != "" {
+		mystreams := retrieveAwsStreams(client, logStreamPreix)
+		// append streams to the list
+		streams = append(streams, mystreams...)
+		// streams = append(streams, mystreams...)
+	} else {
+		streams = append(streams, logStreamName)
+	}
+
+	for _, stream := range streams {
+		retrieveAwsLog(client, stream)
+	}
 }
 
 func readConfigFromCommandLine() {
 
-	var logGroupName, logStreamName, from, duration string
+	var from, duration string
 	var help bool
 
 	flag.StringVar(&region, "r", "", "AWS region")
 	flag.StringVar(&logGroupName, "g", "", "AWS log group name")
-	flag.StringVar(&logStreamName, "s", "", "AWS log stream name")
+	flag.StringVar(&logStreamName, "s", "", "AWS log stream name \n"+
+		"Either Stream Name or Stream Prefix should has value")
+	flag.StringVar(&logStreamPreix, "pre", "", "(Optional) AWS Log stream prefix \n"+
+		"Either Stream Prefix or Stream Name should has value")
 	flag.StringVar(&from, "f", "", "(Optional) From time in RFC3339 format. e.g.: 2024-02-13T14:25:60Z. By default is current time.")
 	flag.StringVar(&duration, "d", "1h", "(Optional) Duration of the log to be taken from the From time. \n"+
 		"Valid time units are \"ns\", \"us\" (or \"µs\"), \"ms\", \"s\", \"m\", \"h\"")
 	flag.StringVar(&profile, "p", "", "(Optional) Profile")
-	flag.BoolVar(&logTimestamp, "t", false, "(Optional) Whether to write the log entry received timestamp (UTC) to the log")
+	flag.BoolVar(&showLogTimestamp, "t", false, "(Optional) Whether to write the log entry received timestamp (UTC) to the log")
 	flag.BoolVar(&help, "h", false, "Help")
+	flag.StringVar(&outfolder, "o", "", "(Optional) Output folder for the log file")
 	flag.Parse()
 
 	if help {
@@ -49,10 +80,11 @@ func readConfigFromCommandLine() {
 		os.Exit(0)
 	}
 
-	if (region == "") || (logGroupName == "") || (logStreamName == "") || (duration == "") {
+	if (region == "") || (logGroupName == "") || (logStreamName == "" && logStreamPreix == "") || (duration == "") {
 		fmt.Fprintln(os.Stderr, "Region: ", region)
 		fmt.Fprintln(os.Stderr, "Group: ", logGroupName)
 		fmt.Fprintln(os.Stderr, "Stream: ", logStreamName)
+		fmt.Fprintln(os.Stderr, "StreamPrefix: ", logStreamPreix)
 		fmt.Fprintln(os.Stderr, "Duration: ", duration)
 		printHelp()
 		fmt.Fprintf(os.Stderr, ERROR_MSG, fmt.Errorf("missing parameters"))
@@ -78,33 +110,83 @@ func readConfigFromCommandLine() {
 	}
 
 	timeTo := timeFrom.Add(d)
-
-	startTime := timeFrom.UnixMilli()
-	endTime := timeTo.UnixMilli()
-
-	startFromHeader := true
-
-	//get log events
-	logEventInput = &cloudwatchlogs.GetLogEventsInput{
-		LogGroupName:  &logGroupName,
-		LogStreamName: &logStreamName,
-		StartTime:     &startTime,
-		EndTime:       &endTime,
-		StartFromHead: &startFromHeader, // Start from the beginning of the log stream
-	}
+	startTime = timeFrom.UnixMilli()
+	endTime = timeTo.UnixMilli()
 }
 
-func retrieveAwsLog() {
+func createAwsClient() (*cloudwatchlogs.Client, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithDefaultRegion(region),
 		config.WithSharedConfigProfile(profile),
 	)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, ERROR_MSG, err)
-		fmt.Fprintf(os.Stderr, "Error when setting up credential\n")
+		//fmt.Fprintf(os.Stderr, ERROR_MSG, err)
+		//fmt.Fprintf(os.Stderr, "Error when setting up credential\n")
+		return nil, err
 	}
+
 	//create cloudwatchlogs client
 	client := cloudwatchlogs.NewFromConfig(cfg)
+	return client, nil
+}
+
+func retrieveAwsStreams(client *cloudwatchlogs.Client, streamPrefix string) []string {
+
+	fmt.Fprintln(os.Stderr, "Retrieving log streams for", streamPrefix)
+
+	input := &cloudwatchlogs.DescribeLogStreamsInput{
+		LogGroupName:        &logGroupName,
+		LogStreamNamePrefix: &streamPrefix,
+	}
+
+	var logStreams []string
+	for {
+		resp, err := client.DescribeLogStreams(context.TODO(), input, func(o *cloudwatchlogs.Options) {
+			o.Region = region
+		})
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, ERROR_MSG, err)
+			os.Exit(1)
+		}
+
+		for _, stream := range resp.LogStreams {
+			if startTime < *stream.LastEventTimestamp && endTime > *stream.CreationTime {
+				logStreams = append(logStreams, *stream.LogStreamName)
+			}
+		}
+
+		if resp.NextToken == nil ||
+			(input.NextToken != nil && *input.NextToken == *resp.NextToken) {
+			// We've reached the end of the stream
+			break
+		}
+
+		input.NextToken = resp.NextToken
+	}
+
+	return logStreams
+}
+
+func retrieveAwsLog(client *cloudwatchlogs.Client, streamName string) {
+
+	//if outFolder is not empty, then write the log to a file
+	file, shouldReturn := createFile(streamName)
+	if shouldReturn {
+		return
+	}
+	defer file.Close()
+
+	startFromHeader := true
+
+	//get log events
+	logEventInput := &cloudwatchlogs.GetLogEventsInput{
+		LogGroupName:  &logGroupName,
+		LogStreamName: &streamName,
+		StartTime:     &startTime,
+		EndTime:       &endTime,
+		StartFromHead: &startFromHeader, // Start from the beginning of the log stream
+	}
 
 	for {
 		resp, err := client.GetLogEvents(context.TODO(), logEventInput, func(o *cloudwatchlogs.Options) {
@@ -117,11 +199,11 @@ func retrieveAwsLog() {
 
 		timestampeLayout := "2006-01-02 15:04:05.000"
 		for _, event := range resp.Events {
-			if logTimestamp {
+			if showLogTimestamp {
 				timestamp := time.UnixMilli(int64(*event.Timestamp)).UTC()
-				fmt.Print(timestamp.Format(timestampeLayout), " ")
+				writeLogEntry(file, timestamp.Format(timestampeLayout)+" ")
 			}
-			fmt.Println(*event.Message)
+			writeLogEntry(file, *event.Message+"\n")
 		}
 
 		if resp.NextForwardToken == nil ||
@@ -131,6 +213,31 @@ func retrieveAwsLog() {
 		}
 
 		logEventInput.NextToken = resp.NextForwardToken
+	}
+}
+
+func createFile(streamName string) (*os.File, bool) {
+	var file *os.File
+	var err error
+
+	if outfolder != "" {
+		fileName := outfolder + "/" + streamName + ".log"
+		fmt.Fprintln(os.Stderr, "creating file: "+fileName)
+		//create the file
+		file, err = os.Create(fileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, ERROR_MSG, err)
+			return nil, true
+		}
+	}
+	return file, false
+}
+
+func writeLogEntry(file *os.File, logEntry string) {
+	if file != nil {
+		file.WriteString(logEntry)
+	} else {
+		fmt.Print(logEntry)
 	}
 }
 
